e1a9daef2f267e520b19dde0eaf0e81b54f8835e
Jarkko Mönkkönen <jamonkko@gmail.com>
2016-05-08 01:19:16 +0300

    support both inline traversal in one go or separate post traversal processing

--- src/index.js
+++ src/index.js
@@ -6,42 +6,46 @@ export default ({ types: t }) => {
       valueNode
     )
 
-  const visitor = ({ renames, process }) => ({
-    AssignmentExpression(path) {
-      if (!process.includes('inline')) {
-        return
+  function visitAssignmentExpression(path, renames) {
+    const { node: { operator, left: memberExpression, right: valueExpression } } = path
+    if (operator !== '=' || !t.isMemberExpression(memberExpression)) {
+      return
+    }
+    for (const object of Object.keys(renames)) {
+      if (!t.isIdentifier(memberExpression.object, { name: object })) {
+        continue
       }
-      const { node: { operator, left: memberExpression, right: valueExpression } } = path
-      if (operator !== '=' || !t.isMemberExpression(memberExpression)) {
-        return
-      }
-      for (const object of Object.keys(renames)) {
-        if (!t.isIdentifier(memberExpression.object, { name: object })) {
+      for (const property of Object.keys(renames[object])) {
+        if (!t.isIdentifier(memberExpression.property, { name: property })) {
           continue
         }
-        for (const property of Object.keys(renames[object])) {
-          if (!t.isIdentifier(memberExpression.property, { name: property })) {
-            continue
-          }
-          const [to, ...aliases] = [].concat(renames[object][property])
-          const renamedAssignment = buildPropertyAssignment(object, to, valueExpression)
-          const chainedAliasAssignments = aliases.reduce(
-            (chained, alias) =>
-              buildPropertyAssignment(object, alias, chained),
-            renamedAssignment
-          )
-          path.replaceWith(t.expressionStatement(chainedAliasAssignments))
-        }
+        const [to, ...aliases] = [].concat(renames[object][property])
+        const renamedAssignment = buildPropertyAssignment(object, to, valueExpression)
+        const chainedAliasAssignments = aliases.reduce(
+          (chained, alias) =>
+            buildPropertyAssignment(object, alias, chained),
+          renamedAssignment
+        )
+        path.replaceWith(t.expressionStatement(chainedAliasAssignments))
       }
     }
-  })
+  }
 
   return {
     visitor: {
+      AssignmentExpression(path, { opts }) {
+        if (!opts.process || opts.process.includes('inline')) {
+          visitAssignmentExpression(path, opts.renames)
+        }
+      },
       Program: {
         exit(program, { opts }) {
-          if (opts.process.includes('post')) {
-            program.traverse(visitor(Object.assign({}, opts, { process: 'inline' })))
+          if (opts.process && opts.process.includes('post')) {
+            program.traverse({
+              AssignmentExpression(path) {
+                visitAssignmentExpression(path, opts.renames)
+              }
+            })
           }
         }
       }
